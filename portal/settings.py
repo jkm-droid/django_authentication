78adbd2d0ec757553656887f894f46b8f3f1a9ea
jkmchinoxxx <31919649+jkmchinoxxx@users.noreply.github.com>
2020-01-07 00:24:45 +0300

    Update settings.py

--- portal/settings.py
+++ portal/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portal',
-        'USER': 'postgres',
-        'PASSWORD': 'mainajoseph2407',
-        'HOST': 'localhost',
-        'PORT': '',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
