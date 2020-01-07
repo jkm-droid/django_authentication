4d2e5b6a699d2fac563ebdb447b999f21ad6175c
jkm2407 <joshlinnas@gmail.com>
2020-01-07 13:42:37 +0300

    another commit

--- portal/settings.py
+++ portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h(ddax$5=88!^r#=r0n+ql*m7w^r)==zob$sslab!1qv@yrudw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','users-portal.herokuapp.com']
 
